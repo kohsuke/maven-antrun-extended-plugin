918617fd559b42f5b7d4e8c9e72ded6d084dff98
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-04-01 23:11:35 +0000

    [maven-release-plugin]  copy for tag maven-antrun-extended-plugin-1.23

--- src/main/java/org/jvnet/maven/plugin/antrun/ResolveAllTask.java
+++ src/main/java/org/jvnet/maven/plugin/antrun/ResolveAllTask.java
@@ -5,13 +5,17 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.Copy;
 import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.Path;
+import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
 import org.jvnet.maven.plugin.antrun.DependencyGraph.Node;
 
 import java.io.File;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * Transitively resolve dependencies, perform some filtering, and deliver the resulting
+ * Transitively resolve dependencies, perform some filtering first on the graph data model,
+ * then on list data model, and deliver the resulting
  * set of artifacts in various forms (as a new {@link Path} object, into a directory, etc.)
  *
  * @author Kohsuke Kawaguchi
@@ -22,6 +26,10 @@ public class ResolveAllTask extends DependencyGraphTask {
     private File todir;
     
     private String pathId;
+
+    private GraphFilter filter;
+
+    private final List<ListFilter> listFilters = new ArrayList<ListFilter>();
     
     public void setTodir(File todir) {
         this.todir = todir;
@@ -32,17 +40,53 @@ public class ResolveAllTask extends DependencyGraphTask {
         this.pathId = pathId;
     }
 
+    /**
+     * Adds a {@link GraphFilter} child. Ant will invoke this for each child element given in build script.
+     */
+    public void add(GraphFilter child) {
+        if(filter==null)
+            this.filter = child;
+        else {
+            if (child instanceof ListFilter) {
+                listFilters.add((ListFilter) child);
+            } else {
+                throw new BuildException(filter+" is not a list filter");
+            }
+        }
+    }
+
     public void execute() throws BuildException {
         log("Starting ResolveAllTasks.execute ", Project.MSG_DEBUG);
-        Collection<Node> nodes = buildGraph().getAllNodes();
+
+        // first graph filtering
+        DependencyGraph g = buildGraph(filter);
+        List<Node> nodes = new ArrayList<Node>(g.getAllNodes());
+
+        // further trim down the list by list filtering
+        final DependencyGraph old = GraphFilter.CURRENT_INPUT.get();
+        GraphFilter.CURRENT_INPUT.set(g);
+        try {
+            for (ListFilter listFilter : listFilters) {
+                for (Iterator<Node> itr = nodes.iterator(); itr.hasNext();)
+                    if(!listFilter.visit(itr.next()))
+                        itr.remove();
+            }
+        } finally {
+            GraphFilter.CURRENT_INPUT.set(old);
+        }
+
 
         if(pathId!=null) {
             // collect all artifacts into a path and export
             Path path = new Path(getProject());
             for (Node n : nodes) {
-                File f = n.getArtifactFile();
-                if(f!=null)
+                try {
+                    File f = n.getArtifactFile();
+                    if(f!=null)
                     path.createPathElement().setLocation(f);
+                } catch (AbstractArtifactResolutionException e) {
+                    throw new BuildException("Failed to resolve artifact. Trail="+n.getTrail(g),e);
+                }
             }
             getProject().addReference(pathId,path);
         }
@@ -57,12 +101,16 @@ public class ResolveAllTask extends DependencyGraphTask {
 
             boolean hasSomethingToCopy=false;
             for (Node n : nodes) {
-                File f = n.getArtifactFile();
-                if(f!=null) {
-                    FileSet fs = new FileSet();
-                    fs.setFile(f);
-                    cp.addFileset(fs);
-                    hasSomethingToCopy=true;
+                try {
+                    File f = n.getArtifactFile();
+                    if(f!=null) {
+                        FileSet fs = new FileSet();
+                        fs.setFile(f);
+                        cp.addFileset(fs);
+                        hasSomethingToCopy=true;
+                    }
+                } catch (AbstractArtifactResolutionException e) {
+                    throw new BuildException("Failed to resolve artifact. Trail="+n.getTrail(g),e);
                 }
             }
 
